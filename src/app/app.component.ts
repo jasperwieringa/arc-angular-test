645af7dbc3d82b56c66fac5c3ba5cafd929f5c1f
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 17:08:34 +0100

    Removed unused imports.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import '@arc-web/components/dist/components/container/arc-container.js';
 import '@arc-web/components/dist/components/navbar/arc-navbar.js';
 import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
-import '@arc-web/components/dist/components/button/arc-button.js';
 
 @Component({
   selector: 'app-root',
