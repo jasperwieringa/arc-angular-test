439e2f7cce6c9478d72865181a1af2005b5ba08e
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 17:06:20 +0100

    Updated the Arc-Angular demo.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { setBasePath } from "@arc-web/components/dist/utilities/base-path.js";
+setBasePath('/');
+
 if (environment.production) {
   enableProdMode();
 }
